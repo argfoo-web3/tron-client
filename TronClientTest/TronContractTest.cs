5cb018b07c3627ef1ce2f218cdf98d8ec7e08bad
zhifenglee-portkey <134916307+zhifenglee-portkey@users.noreply.github.com>
2024-01-10 10:10:26 +0800

    Update TronContractTest with Nethereum library
    The TronContractTest has been refactored with the use of the Nethereum library for Ethereum based contracts. This update includes replacements of FunctionOutput and Parameters attributes, changes in test methods, as well as the introduction of 'SymbolFunctionMessage', 'DecimalsFunctionMessage', and 'CommitFunctionMessage' classes. New tests for these functions have also been added.

--- TronClientTest/TronContractTest.cs
+++ TronClientTest/TronContractTest.cs
@@ -1,11 +1,12 @@
 using System.Numerics;
 using HDWallet.Tron;
+using Nethereum.Contracts;
+using Nethereum.Contracts.MessageEncodingServices;
 using TronClient;
 using TronNet;
 using TronNet.ABI;
-using TronNet.ABI.FunctionEncoding;
-using TronNet.ABI.FunctionEncoding.Attributes;
-using Parameter = TronNet.ABI.Model.Parameter;
+using TronNet.Crypto;
+using IFunctionOutputDTO = TronNet.ABI.FunctionEncoding.Attributes.IFunctionOutputDTO;
 
 namespace TronClientTest
 {
@@ -20,29 +21,39 @@ namespace TronClientTest
             _tronClient = new TronClient.TronClient(ApiUrl, "");
         }
         
-        [FunctionOutput]
+        [Nethereum.ABI.FunctionEncoding.Attributes.Function("symbol", "tuple[]")]
+        public class SymbolFunctionMessage: FunctionMessage
+        {
+        }
+        
+        [TronNet.ABI.FunctionEncoding.Attributes.FunctionOutput]
         public class SymbolDto : IFunctionOutputDTO
         {
-            [Parameter("string", 1)]
+            [TronNet.ABI.FunctionEncoding.Attributes.Parameter("string", 1)]
             public string Symbol { get; set; }
         }
-
+        
         [Test]
         public async Task Test_TronTriggerConstantContractSymbol()
         {
             var usdtContract = _tronClient.GetContract("TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs");
-            var result = await usdtContract.CallAsync<SymbolDto>(new TronConstantContractFunctionMessage
+            var result = await usdtContract.CallAsync<SymbolFunctionMessage, SymbolDto>(new TronConstantContractFunctionMessage<SymbolFunctionMessage>
             {
-                FunctionSelector = "symbol()"
+                FunctionMessage = new SymbolFunctionMessage()
             });
             
             Assert.That(result.Symbol, Is.EqualTo("USDT"));
         }
         
-        [FunctionOutput]
+        [Nethereum.ABI.FunctionEncoding.Attributes.Function("decimals", "tuple[]")]
+        public class DecimalsFunctionMessage: FunctionMessage
+        {
+        }
+        
+        [TronNet.ABI.FunctionEncoding.Attributes.FunctionOutput]
         public class DecimalsDto : IFunctionOutputDTO
         {
-            [Parameter("uint256", 1)]
+            [TronNet.ABI.FunctionEncoding.Attributes.Parameter("uint256", 1)]
             public BigInteger Decimals { get; set; }
         }
         
@@ -50,12 +61,19 @@ namespace TronClientTest
         public async Task Test_TronTriggerConstantContractDecimals()
         {
             var usdtContract = _tronClient.GetContract("TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs");
-            var response = await usdtContract.CallAsync<DecimalsDto>(new TronConstantContractFunctionMessage
+            var response = await usdtContract.CallAsync<DecimalsFunctionMessage, DecimalsDto>(new TronConstantContractFunctionMessage<DecimalsFunctionMessage>
             {
-                FunctionSelector = "decimals()"
+                FunctionMessage = new DecimalsFunctionMessage()
             });
 
             Assert.That((int)response.Decimals, Is.EqualTo(6));
+        }
+        
+        [Nethereum.ABI.FunctionEncoding.Attributes.Function("commit", "tuple[]")]
+        public class CommitFunctionMessage: FunctionMessage
+        {
+            [Nethereum.ABI.FunctionEncoding.Attributes.Parameter("address", "owner", 1)]
+            public string Owner { get; set; }
         }
         
         [Test, Ignore("This test costs gas fees on shasta!")]
@@ -64,12 +82,16 @@ namespace TronClientTest
             const string privateStr = "559e591bac0c8b1e039901b127a46a02443b05cf73abbd41c802038e14151fe3";
             var tronWallet = new TronWallet(privateStr);
             
-            var contract = _tronClient.GetContract("TMEmbYxxnAEWpfC1PNaVBx78Y5c2wKDaxp");
-            var response = await contract.SendAsync(tronWallet, new TronSmartContractFunctionMessage
+            var commitFunctionMessage = new CommitFunctionMessage
             {
-                FunctionSelector = "commit(address)",
+                Owner = TronAddressToHex(tronWallet.Address)
+            };
+            
+            var contract = _tronClient.GetContract("TMEmbYxxnAEWpfC1PNaVBx78Y5c2wKDaxp");
+            var response = await contract.SendAsync(tronWallet, new TronSmartContractFunctionMessage<CommitFunctionMessage>
+            {
+                FunctionMessage = commitFunctionMessage,
                 FeeLimit = 10000000,
-                Parameters = new KeyValuePair<string, string>[] { new("address", tronWallet.Address) },
                 CallValue = 0,
                 CallTokenValue = 0,
                 TokenId = 0,
@@ -77,6 +99,44 @@ namespace TronClientTest
             });
             
             Assert.That(response.result, Is.True);
+        }
+
+        [Nethereum.ABI.FunctionEncoding.Attributes.Function("getSendReceiptIndex", "tuple[]")]
+        public class GetSendReceiptIndexFunctionMessage: FunctionMessage
+        {
+            [Nethereum.ABI.FunctionEncoding.Attributes.Parameter("address[]", "tokens", 1)]
+            public List<string> Tokens { get; set; }
+    
+            [Nethereum.ABI.FunctionEncoding.Attributes.Parameter("string[]", "targetChainIds", 2)]
+            public List<string> TargetChainIds { get; set; }
+        }
+        
+        [Test]
+        public void Test_FunctionMessage()
+        {
+            var contractFunctionMessage = new GetSendReceiptIndexFunctionMessage();
+            contractFunctionMessage.Tokens = new List<string>
+            {
+                TronAddressToHex("TMEmbYxxnAEWpfC1PNaVBx78Y5c2wKDaxp")
+            };
+            contractFunctionMessage.TargetChainIds = new List<string>
+            {
+                "Shasta"
+            };
+            
+            var functionMessageEncodingService = new FunctionMessageEncodingService<GetSendReceiptIndexFunctionMessage>();
+            functionMessageEncodingService.SetContractAddress("contractAddress");
+            functionMessageEncodingService.DefaultAddressFrom = null;
+            var callInput = functionMessageEncodingService.CreateCallInput(contractFunctionMessage);
+            
+            Assert.That(callInput.Data, Is.Not.Null);
+        }
+        
+        private static string TronAddressToHex(string value)
+        {
+            var addressByte = Base58Encoder.DecodeFromBase58Check(value);
+            addressByte = addressByte.Slice(1, addressByte.Length);
+            return addressByte.ToHex();
         }
     }
 }
