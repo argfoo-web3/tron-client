4ee0341cb5dfce967edf7f70d1cfce79026ab654
zhifenglee-portkey <134916307+zhifenglee-portkey@users.noreply.github.com>
2024-01-10 10:10:18 +0800

    Add 'data' field to TriggerConstantContractRequest
    A new 'data' field has been added to the TriggerConstantContractRequest class. Additionally, the 'function_selector' field has been marked as nullable to improve error handling and prevent exceptions if it's not provided.

--- TronClient/Request/Types/TriggerConstantContractRequest.cs
+++ TronClient/Request/Types/TriggerConstantContractRequest.cs
@@ -4,8 +4,9 @@ namespace TronClient.Request.Types
     {
         public string owner_address;
         public string contract_address;
-        public string function_selector;
+        public string? function_selector;
         public string? parameter;
+        public string data;
         public bool visible;
     }
 }
