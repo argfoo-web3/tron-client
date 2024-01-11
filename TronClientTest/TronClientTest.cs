57dbe61d97404989ef762644e21b0e56623fd527
zhifenglee-portkey <134916307+zhifenglee-portkey@users.noreply.github.com>
2024-01-11 21:38:00 +0800

    Add additional tests to TronClientTest
    New unit tests have been added to the TronClientTest.cs file. These tests focus on fetching transaction and block details from the Tron network, specifically by transaction ID and block number. This will strengthen our test coverage for the Tron client by verifying that it handles those tasks appropriately.

--- TronClientTest/TronClientTest.cs
+++ TronClientTest/TronClientTest.cs
@@ -19,5 +19,26 @@ namespace TronClientTest
             var nowBlock = await _tronClient.GetNowBlockAsync();
             Assert.That(nowBlock.block_header.raw_data.number, Is.GreaterThan(0));
         }
+        
+        [Test]
+        public async Task Test_TronGetTransactionById()
+        {
+            var transaction = await _tronClient.GetTransactionByIdAsync("7c2d4206c03a883dd9066d620335dc1be272a8dc733cfa3f6d10308faa37facc");
+            Assert.That(transaction.ret[0].contractRet, Is.EqualTo("SUCCESS"));
+        }
+        
+        [Test]
+        public async Task Test_TronGetTransactionInfoById()
+        {
+            var transactionInfo = await _tronClient.GetTransactionInfoByIdAsync("7c2d4206c03a883dd9066d620335dc1be272a8dc733cfa3f6d10308faa37facc");
+            Assert.That(transactionInfo.blockNumber, Is.EqualTo(32880248));
+        }
+        
+        [Test]
+        public async Task Test_TronGetBlockByNum()
+        {
+            var block = await _tronClient.GetBlockByNumAsync(32880248);
+            Assert.That(block.block_header.raw_data.number, Is.EqualTo(32880248));
+        }
     }
 }
